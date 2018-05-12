df241255fc57bf7a6889932cc98baf52fbc59982
jee <jag918@nyu.edu>
2018-05-12 03:49:44 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,52 @@
-def reformat_languages(languages)
-  # your code here
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
+def reformat_languages(language)
+  new_hash={}
+  language.each do |key,value|
+    #puts (key)
+    value.each do |attribute,i_or_c|
+      #puts (attribute)
+      puts (i_or_c)
+      new_hash[attribute.to_s] = {i_or_c}
+      #puts (new_hash)
+    #   if key==":oo".to_sym
+    #     new_hash[attribute.to_s]={style:":oo"}
+    #   else
+    #     new_hash[attribute.to_s]={style:":functional"}
+    #   end
+    end
+  end
+new_hash
 end
+
+reformat_languages(languages)
